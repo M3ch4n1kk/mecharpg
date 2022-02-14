3560041b55212f42c26be8a238cc4947b9be0cf8
mecha does code <69651144+M3ch4n1kk@users.noreply.github.com>
2022-02-14 02:48:02 +0100

    Add files via upload

--- rpgtest.py
+++ rpgtest.py
@@ -3,176 +3,375 @@ import math
 import json
 from time import sleep
 import pygame
+import os
+from cryptography.fernet import Fernet
+import base64
+
+def clear():
+    os.system('cls')
+
+
+enemy = 0
+ehp = 0
+edmg = 0
+e_info = ""
+enemyAlive = False
+block = False
+
+p_data = {"hp":100,"maxhp":100,"resistance":0,"strength":0,"xp":0,"lvl":1,"lvlnext":25,"pinespag":3,"explcard":3,"audio_music":50,"audio_sfx":50,"biome":0} # player save data
+recovery_data = p_data
+
 
 pygame.init()
 pygame.mixer.init()
 damage_sound = pygame.mixer.Sound("media/damage.wav")
-damage_sound.set_volume(0.2)
-
-
-biome = 0
-enemy = 0
-ehp = 0
-enemyAlive = False
-block = False
-
-p_inv = {"pinespag":3,"explcard":3} # player inventory
-
-p_st = {"hp":100,"maxhp":100,"armor":1,"strength":1,"xp":99999,"lvl":0,"lvlnext":25} # player stats
+damage_sound.set_volume(p_data["audio_sfx"]/100)
 
 
 def save(): # saves game data
     data = open("data/playerdata.json", "w")
-    data = json.dump(p_st, data)
+    data = json.dump(p_data, data)
 
 
 def load(): # loads game data on startup
     data = open("data/playerdata.json", "r")
-    p_st = json.load(data)
+    p_data = json.load(data)
+
+
+def quit():
+    clear()
+    savegame = input("\nWould you like to save your game? (y/n)\n")
+
+    if savegame.lower() == "y":
+        save()
+        print("\nSaving...")
+        sleep(1)
+        exit()
+
+    elif savegame.lower() == "n":
+        print ("\nClosing...")
+        sleep(1)
+        exit()
+
+    else:
+        print("\nInvalid option. Please choose one of the options to the right")
+        quit()
 
 
 def xpsystem():
-    global p_st
-    while p_st["xp"] >= p_st["lvlnext"]:
-        p_st["lvl"] += 1
-        p_st["strength"] += 0.1
-        p_st["maxhp"] = round(p_st["maxhp"]*1.1)
-        p_st["hp"] = p_st["maxhp"]
-        p_st["xp"] -= p_st["lvlnext"]
-        p_st["lvlnext"] = round(p_st["lvlnext"]*1.5)
-        print("Your level is now {lvl}!\n".format(**p_st))
+    global p_data
+    while p_data["xp"] >= p_data["lvlnext"]:
+        p_data["lvl"] += 1
+        p_data["strength"] = round(p_data["strength"]*1.1)
+        p_data["maxhp"] = round(p_data["maxhp"]*1.1)
+        p_data["xp"] -= p_data["lvlnext"]
+        p_data["lvlnext"] = round(p_data["lvlnext"]*1.5)
+        input("\nYour level is now {lvl}!".format(**p_data))
 
 
 def plaindlg():
-    input("\nThe lush green grass dances in motion as the gust of wind brushes itself against the empty hills.")
+    input("The lush green grass dances in motion as the gust of wind brushes itself against the empty hills.")
     input("You keep walking as you see the nearest town getting engulfed by the horizon behind you.")
-    input("Suddenly, a silhoulette appears front of you.")
+    clear()
 
 def mountdlg():
-    input("\nThe peaks reach far out into the sky. Fortunately, you're almost at the top.")
+    input("The peaks reach far out into the sky. Fortunately, you're almost at the top.")
     input("You feel a sharp blizzard cutting your face, but that doesn't stop you from climbing any further.")
-    input("After reaching the top of the tall cliff, you find a giant cave opening.\n")
+    input("After reaching the top of the tall cliff, you find a giant cave opening.")
 
 def mountdlg2():
     input("As you venture deeper into the cave, some strange crystals on the walls start to glow.")
     input("The cave lits up, revealing an unidentified entity.")
-    input("You have no other choice than to fight it.\n")
+    input("You have no other choice than to fight it.")
+
+
+def seed():
+    seed = random.randint(0,1)
+    if seed == 0:
+        input("You see an enemy in the distance.")
+        spawn()
+
+    elif seed == 1:
+        input("You found an item!")
+        if p_data["biome"] == 0:
+            item = 0 # random.randint(0,1)
+            if item == 0:
+                p_data["pinespag"] += 1
+                input("You found a Pineapple Spaghetti!")
+                clear()
+            
+            else:
+                input("Nevermind, it was just the wind.")
+                clear()
 
 
 def spawn():
+    global p_data,block,ehp,e_info,edmg
     choice = input("Will you approach it? (y/n)\n")
-    if choice == "y":
-        if biome == 0:
+    if choice.lower() == "y":
+        if p_data["biome"] == 0:
             enemy = 0
 
             if enemy == 0:
-                enemyAlive = True
-                global p_st,p_inv,block
                 ehp = 200
-                input("\nYou encountered a GIANT SLIME! 💀\n")
+                edmg = 40
+                e_info = f"\n--- Giant Slime ---\nAlthough a mere couple strikes with a sword can do the job, their blow is quite devastating.\nHP: 200\nATK: 40\n"
+                input("\nYou encountered a GIANT SLIME!\n")
 
-                while(enemyAlive):
-                    action = input("Fight | Item | Block | Info >\n")
-                    if action == "fight":
-                        input("\nYou attacked the slime.")
-                        dmg = math.trunc(random.randrange(120,150)*p_st["strength"])
-                        ehp -= dmg
-
-                        if (ehp)<0:
-                            ehp = 0
+                while(ehp>0):
+                    action()
                         
-                        sleep(0.5)
-                        damage_sound.play()
-                        input(f"You dealt {dmg} damage! {ehp}/200\n")
-                        if (ehp)>0:
-                            pass
-                        else:
-                            p_st["xp"] += 100
-                            input("You won! You gained 100 XP!\n")
-                            xpsystem()
-                            input()
-                            enemyAlive = False
-
-                    elif action == "item":
-                        item = input("\nPineSpag ({p_inv[pinespag]}) | ExplCard ({p_inv[explcard]})> ")
-                        if item == "pinespag":
-                            if p_inv["pinespag"]>0:
-                                p_inv["pinespag"] -= 1
-                                p_st["hp"] += 80
-
-                                if p_st["hp"]>p_st["maxhp"]:
-                                    p_st["hp"] = p_st["maxhp"]
-
-                                input("You ate Pineapple Spaghetti. You restored 80 health. {hp}/{maxhp}".format(**p_st))
-                                if p_st["hp"]==p_st["maxhp"]:
-                                    input("Your health has been maxed out.\n")
-
-                            else:
-                                input("You don't have any Pineapple Spaghetti left.\n")
-                        
-                    elif action == "block":
-                        block = True
-                        input("You blocked the next attack.\n")
-                    elif action == "info":
-                        input("- Giant Slime -\nAlthough a mere couple strikes with a sword can do the job, their blow is quite devastating.\nHP: 200\nATK: 40\n")
-                    else:
-                        input(f"There's no '{action}' option.\n")
-                        
-                    if enemyAlive == True:
+                    if ehp>0:
                         input("Giant Slime attacks you.")
                         if block == False:
-                            dmg = 40
+                            dmg = edmg
                         else:
-                            dmg = math.trunc(40*0.3)
-                        p_st["hp"] -= dmg
-                        if p_st["hp"]<0:
-                            p_st["hp"] = 0
-                        input(f"You took {dmg} damage!" + " {hp}/{maxhp}\n".format(**p_st))
+                            dmg = math.trunc(str(edmg*0.3))
+                        p_data["hp"] -= dmg
+                        if p_data["hp"]<0:
+                            p_data["hp"] = 0
+                        input(f"You took {dmg} damage!" + " {hp}/{maxhp}\n".format(**p_data))
                         block = False
-                        if p_st["hp"]>0:
+                        if p_data["hp"]>0:
                             pass
                         else:
+                            ehp = 0
                             input("You died!\n")
-                            input("Exiting...")
-                            sleep(1)
-    elif choice == "n":
-        input("You decided not to approach the thing.\n")
+                            print("Closing...")
+                            exit()
+            else:
+                input("\nNevermind, there was nothing.")
+                clear
+
+        elif p_data["biome"] == 1:
+            enemy = 0
+
+
+    elif choice.lower() == "n":
+        input("\nYou decided not to approach the thing.")
+
     else:
-        input("Invalid option. Please choose one of the options to the right.\n")
+        input("\nSyntax error: not a valid option.")
         spawn()
 
+
+def action():
+    isRunning = True
+    while(isRunning):
+        clear()
+        action = input("Fight | Item | Block | Info >\n")
+        if action.lower() == "fight" or action == "1":
+            action_fight()
+            isRunning = False
+
+        elif action.lower() == "item" or action == "2":
+            action_item()
+            
+        elif action.lower() == "block" or action == "3":
+            action_block()
+            isRunning = False
+
+        elif action.lower() == "info'" or action == "4":
+            action_info()
+
+        else:
+            input(f"\nThere's no '{action}' option.\n")
+
+def action_fight():
+    global ehp,isRunning
+    clear()
+    input("You attacked the slime.")
+    dmg = math.trunc(random.randrange(120,150)+(p_data["strength"]))
+    ehp -= dmg
+
+    if ehp<0:
+        ehp = 0
+    
+    sleep(0.5)
+    damage_sound.play()
+    input(f"You dealt {dmg} damage! {ehp}/200\n")
+    if ehp>0:
+        pass
+
+    else:
+        isRunning = False
+        p_data["xp"] += 100
+        input("You won! You gained 100 XP!")
+        xpsystem()
+
+
+def action_item():
+    global isRunning
+    clear()
+    item = input("PineSpag ({pinespag}) | ExplCard ({explcard}) > ".format(**p_data))
+    if item.lower() == "pinespag":
+        if p_data["pinespag"]>0:
+            p_data["pinespag"] -= 1
+            p_data["hp"] += 80
+
+            if p_data["hp"]>p_data["maxhp"]:
+                p_data["hp"] = p_data["maxhp"]
+
+            clear()
+            input("You ate Pineapple Spaghetti. You restored 80 health. {hp}/{maxhp}".format(**p_data))
+            if p_data["hp"] == p_data["maxhp"]:
+                input("Your health has been maxed out.\n")
+                isRunning = False
+
+        else:
+            clear()
+            input("You don't have any Pineapple Spaghetti left.\n")
+
+    elif item.lower() == "explcard":
+        input("This item has no use yet. ;-;")
+
+    elif item == "":
+        clear()
+
+def action_block():
+    global block
+    block = True
+    clear()
+    input("You blocked the next attack.\n")
+
+def action_info():
+    global ehp
+    input(e_info)
+
+
+def menu_gui():
+    isRunning = True
+    while (isRunning):
+        clear()
+        action = input("--- Menu ---\nStats | Items | Shop\nExplore | Settings | Quit > ")
+
+        if action.lower() == "stats":
+            stat_gui()
+
+        elif action.lower() == "items":
+            inv_gui()
+
+        elif action.lower() == "shop":
+            input("\nShop closed...")
+
+        elif action.lower() == "explore":
+            isRunning = False
+            input("\nYou decided to explore a bit more...\n")
+            sleep(1)
+            seed()
+        
+        elif action.lower() == "settings":
+            settings = input("\n--- Settings ---\nAudio | Save > ")
+
+            if settings.lower() == "audio":
+                clear()
+                set_audio = input("\n--- Audio Settings ---\nMusic: {audio_music}\nSFX: {audio_sfx}\n> ".format(**p_data))
+
+                if set_audio.lower() == "music":
+                    p_data["audio_music"] = int(input("Change music volume: "))
+                    input("\nMusic set to {audio_music}".format(**p_data))
+                    clear()
+
+                elif set_audio.lower() == "sfx":
+                    p_data["audio_sfx"] = int(input("Change SFX volume: "))
+                    input("\nSFX set to {audio_sfx}".format(**p_data))
+                    clear()
+            elif settings.lower() == "save":
+                savegame()
+
+            elif settings == "":
+                pass
+
+            else:
+                input("Invalid option.")
+
+        elif action == "quit":
+            quit()
+
+        else:
+            input("\nSyntax error: not a valid option.")
+
+
 def stat_gui():
-    global p_st
-    input("--- Player Statistics ---\nHealth: {hp}\nLevel: {lvl}\nExperience: {xp}\nNext Level: {lvlnext}\nArmor: {armor}\nStrength: {strength}\n".format(**p_st))
+    input("\n--- Player Statistics ---\nHealth: {hp}/{maxhp}\nLevel: {lvl}\nExperience: {xp}\nNext Level: {lvlnext}\nResistance: ".format(**p_data) + str(p_data["resistance"]) +"\nStrength: " + str(p_data["strength"]) + "\n")
+
+def inv_gui():
+    input("\nPineSpag ({pinespag}) | ExplCard ({explcard}) > ".format(**p_data))
+
+def savegame():
+    saveRunning = True
+    while(saveRunning):
+        clear()
+        savegame = input("\n--- Save Settings ---\nSave Game | Delete Save | Close > ")
+
+        if savegame.lower() == "save":
+            save()
+            input("\nGame saved!")
+
+        elif savegame.lower() == "delete":
+            yesno = input("\nAre you sure you want to permanently delete your save data? (y/n)\n")
+
+            if yesno.lower() == "y":
+                print ("\nDeleting save file...")
+                sleep(1)
+                data = open("data/playerdata.json", "w")
+                data = json.dump(recovery_data, data)
+                input("Save file successfully deleted!")
+                print("Closing...")
+                sleep(1)
+                exit()
+
+            elif yesno.lower() == "n":
+                pass
+
+            else:
+                input("\nInvalid option.")
+
+        elif savegame == "":
+            saveRunning = False
+
+        else:
+            input("\nInvalid option. Please choose one of the options above.")
+
 
 
 # game start
 
 load()
+clear()
 xpsystem()
-welcome = input("Welcome to M3ch4n1kk's RPG test! Would you like to play? (y/n)\n")
+welcome = input("Welcome to M3ch4n1kk's RPG test v0.2.0(alpha)! Would you like to play? (y/n)\n")
 
-if welcome == "y":
-    input("\nYou went through a portal, not knowing where you'll end up.")
-    biome = 0
-    if biome == 0:
-        input("\n--- Biome: Plains ---")
-        plaindlg()
-        spawn()
-        
-    elif biome == 1:
+if welcome.lower() == "y":
+    input("\nYou went through a portal, not knowing where you'll end up...\n")
+    clear()
+    sleep(1)
+    p_data["biome"] = 0
+    if p_data["biome"] == 0:
+        input("--- Biome: Plains ---\n")
+        gameRunning = True
+        while(gameRunning):
+            menu_gui()
+
+
+    elif p_data["biome"] == 1:
         input("--- Biome: Mountains ---")
         mountdlg()
-        choice = input("Go inside the cave? (yes/no) ")
-        if choice == "yes":
+        choice = input("Go inside the cave? (y/n) ")
+        if choice.lower() == "y":
             mountdlg2()
-        else:
-            input("You decide not to go in.")
+            gameRunning = True
+            while(gameRunning):
+                menu_gui()
+
+        elif choice.lower() == "n":
+            input("\nYou decide not to go in.")
             input("Your hand slips from the cliff's edge, and fall to the ground.\nYou Died!")
+            quit()
 
 
 
-    elif biome == 2:
+    elif p_data["biome"] == 2:
         input("--- Biome: Forest ---")
         spawn()
 
@@ -181,30 +380,12 @@ if welcome == "y":
     else:
         input("--- Biome: Underworld ---")
 
-        
-    
 
-elif welcome == "n":
-    input("See you next time then!")
+elif welcome.lower() == "n":
+    input("\nSee you next time then!")
 
 else:
-    input("I didn't quite get that. Try restarting the program.")
+    input("\nI didn't quite get that. Try restarting the program.")
+    exit()
 
-
-def savegame():
-    savegame = input("Would you like to save your game? (y/n)")
-
-    if savegame == "y":
-        save()
-        print("Saving...")
-        sleep(1)
-
-    elif savegame == "n":
-        print ("Closing...")
-        sleep(1)
-
-    else:
-        print("Invalid option. Please choose one of the options to the right")
-        savegame()
-
-savegame()
+quit()
